081ba4e13c856f56291bb30d6165e4742b96fe5f
Diego Sanchez <dgsanchez@synopsis.ws>
2020-01-07 15:17:25 -0500

    Add MONGO_URI Environment

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://dsanchez:7vA1xSFhh5kNNwdz@cluster0-mm5oq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
